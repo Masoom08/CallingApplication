6f276ebed33ca4cd93c93b1ae2fbe6421afc15cd
Masoom08 <masoomsingh0801@gmail.com>
2025-01-28 21:37:58 +0530

    version 1.2.2

--- app/src/main/java/st/masoom/callingapplication/MainActivity.kt
+++ app/src/main/java/st/masoom/callingapplication/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.rememberNavController
+import st.masoom.callingapplication.Navigation.NavigationGraph
 import st.masoom.callingapplication.View.BaseLayout
 import st.masoom.callingapplication.View.ContactListApp
 import st.masoom.callingapplication.ViewModel.ContactViewModel
@@ -24,15 +27,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CallingApplicationTheme {
-                BaseLayout{
+                val navController = rememberNavController()
+/*
+                BaseLayout(navController = navController){
                     val viewModel = ViewModelProvider(
                         this,
                         ContactViewModelFactory(applicationContext)
                     ).get(ContactViewModel::class.java)
-                    ContactListApp(viewModel)
+                    ContactListApp(viewModel,navController = navController,)
                 }
+
+ */
+
+                val viewModel = ViewModelProvider(
+                    this,
+                    ContactViewModelFactory(applicationContext)
+                ).get(ContactViewModel::class.java)
+                //ContactListApp(viewModel,navController = navController,)
+                val contacts = viewModel.contacts.collectAsState(initial = emptyList())
+                NavigationGraph(navController = navController, contacts = contacts.value)
+
+
             }
         }
     }
 }
-
